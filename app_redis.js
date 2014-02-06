92a43246abbd9fd293ca936d8cb9ad2370f02ee6
Chetan <chetan@vouchercodesuae.com>
2014-02-06 20:08:49 +0530

    Redis commit

--- app_redis.js
+++ app_redis.js
@@ -5,12 +5,24 @@ var server = net.createServer(function(conn){
 	console.log('connected');
 	
 	//create Redis client
+
 	var client = redis.createClient();
 	
 	client.on("error",function(err){
 		console.log("Error "+ err);
 	});
 
+	//fifth database is game score database
+	client.select(5);
+	conn.on('data',function(data){
+			console.log(data + ' from '+ conn.remoteAddress + ' '+conn.remotePort);
+
+		try{
+		}catch(err){
+			var obj = JSON.parse(data);
+			console.log("OBJ "+obj);
+		}
+	});
 
 }).listen(8124);
 console.log("Listening to port 8124");
