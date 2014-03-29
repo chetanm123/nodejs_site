8c63c3e3a215c44e1bd72d0012669a0331d595ed
Chetan <chetan@vouchercodesuae.com>
2014-03-29 17:44:45 +0530

    latest changes

--- chat.js
+++ chat.js
@@ -1,29 +1,19 @@
-/*net = require('net');
-sockets=[];
-var s = net.Server(function(socket){
-		sockets.push(socket);
-		socket.on('data',function(d){
-			for(var i=0;i<sockets.length;i++)
-			{
-				sockets[i].write(d);
-			}
-		});
-
-		socket.on('end',function(){
-			var i = sockets.indexOf(socket);
-			sockets.splice(i,1);
-		});
-	});
-*/
 var net = require('net');
 var sockets=[];
 socket = net.createServer(function(socket){
 	sockets.push(socket);
-	
+
 	socket.on('data',function(d){
-		for (var i=0;i<sockets.length;i++){
+		console.log("Remote Address "+socket.remoteAddress+" Remote Port "+socket.remotePort);
+		/*for (var i=0;i<sockets.length;i++){
+			if(sockets[i]==socket)continue;
 			sockets[i].write(d);
-		}
+		}*/
+	});
+
+	socket.on('end',function(){
+		var i = sockets.indexOf(socket);
+		sockets.splice(i,1);
 	});
 });
 socket.listen(8124);
